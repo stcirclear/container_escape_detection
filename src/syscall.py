367efefca6b9a2df4cb36071bd06496bcac5dd64
yzx <1433481694@qq.com>
2023-03-02 23:17:03 -0800

    add pid filter add syscall count

--- src/syscall.py
+++ src/syscall.py
@@ -1,9 +1,10 @@
 from pylcc.lbcBase import ClbcBase, CeventThread
 from syscallTable import *
+import argparse
 import sys
 
 
-bpfProg=r"""
+bpfProg = r"""
 #include "lbc.h"
 #define TASK_COMM_LEN 16
 
@@ -19,10 +20,10 @@ SEC("raw_tracepoint/sys_enter")
 int tp_raw_syscall_sys_enter(struct bpf_raw_tracepoint_args *ctx){
     struct event_t event = {};
     struct task_struct *task = (struct task_struct *)bpf_get_current_task();
-
+    
     event.host_pid = bpf_get_current_pid_tgid() >> 32;
     event.host_ppid = BPF_CORE_READ(task, real_parent, tgid);
-
+    
     bpf_get_current_comm(&event.comm, TASK_COMM_LEN);
     long syscall_id = ctx->args[1];
     event.syscall_id = syscall_id;
@@ -39,15 +40,30 @@ class CtraceSysEnter(ClbcBase):
         super(CtraceSysEnter, self).__init__("syscall", bpf_str=bpfProg)
 
     def _cb(self, cpu, e):
-        print("cpu:%d\thost pid:%d\thost ppid:%d\tcomm:%s\tsyscall id:%ld\tsyscall name:%s" % (
-            cpu, e.host_pid, e.host_ppid, e.comm, e.syscall_id, get_syscall_name(e.syscall_id)
-         ))
+        # monitor parent process and it's child processes
+        if len(pid_set) == 0 or e.host_ppid in pid_set:
+            pid_set.add(e.host_pid)
+            cal_syscall_count(e.syscall_id)
+            print("cpu:%d\thost pid:%d\thost ppid:%d\tcomm:%s\tsyscall id:%ld\tsyscall name:%s\tsyscall count:%d" % (
+                cpu, e.host_pid, e.host_ppid, e.comm, e.syscall_id, get_syscall_name(e.syscall_id), get_syscall_count()[e.syscall_id]
+            ))
 
     def loop(self):
         CeventThread(self, 'e_out', self._cb)
         self.waitInterrupt()
 
 
+def get_options(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description="Parses command.")
+    parser.add_argument("-pp", "--ppid", default=-1, type=int, help="Parent process id.")
+    options = parser.parse_args(args)
+    return options
+
+
 if __name__ == "__main__":
+    options = get_options()
+    pid_set = set()
+    if options.ppid != -1:
+        pid_set.add(options.ppid)
     e = CtraceSysEnter()
     e.loop()
