66d71a5a5ec9c445761ec46dfc6a5c1a5060a1a3
yzx <1433481694@qq.com>
2023-03-24 00:08:18 -0700

    adjust file structure

--- src/process/.output/bpftool/bootstrap/json_writer.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu-18/container_escape_detection/src/process/.output/bpftool/bootstrap/json_writer.o: \
- json_writer.c json_writer.h \
- /home/ubuntu-18/container_escape_detection/bpftool/include/linux/compiler.h \
- /home/ubuntu-18/container_escape_detection/bpftool/include/linux/compiler_types.h \
- /home/ubuntu-18/container_escape_detection/bpftool/include/linux/compiler-gcc.h
