e6deb2f756d024d5a3d0a94ab18e14d24faddf3b
yzx <1433481694@qq.com>
2023-03-23 23:43:03 -0700

    adjust the file structure

---
+++ src/file/.output/bpftool/bootstrap/disasm.d
@@ -0,0 +1,12 @@
+/home/ubuntu-18/container_escape_detection/src/file/.output/bpftool/bootstrap/disasm.o: \
+ /home/ubuntu-18/container_escape_detection/bpftool/src/kernel/bpf/disasm.c \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/uapi/linux/bpf.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/types.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/uapi/linux/bpf_common.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/src/kernel/bpf/disasm.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/kernel.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/build_bug.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/compiler.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/compiler_types.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/compiler-gcc.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/stringify.h
---
+++ src/file/.output/bpftool/bootstrap/xlated_dumper.d
@@ -0,0 +1,25 @@
+/home/ubuntu-18/container_escape_detection/src/file/.output/bpftool/bootstrap/xlated_dumper.o: \
+ xlated_dumper.c \
+ /home/ubuntu-18/container_escape_detection/src/file/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/uapi/linux/bpf.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/types.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/uapi/linux/bpf_common.h \
+ /home/ubuntu-18/container_escape_detection/src/file/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf_common.h \
+ /home/ubuntu-18/container_escape_detection/src/file/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf_version.h \
+ /home/ubuntu-18/container_escape_detection/src/file/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf_legacy.h \
+ /home/ubuntu-18/container_escape_detection/src/file/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf_internal.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/err.h \
+ /home/ubuntu-18/container_escape_detection/src/file/.output/bpftool/bootstrap/libbpf/include/bpf/relo_core.h \
+ /home/ubuntu-18/container_escape_detection/src/file/.output/bpftool/bootstrap/libbpf/include/bpf/libbpf.h \
+ /home/ubuntu-18/container_escape_detection/src/file/.output/bpftool/bootstrap/libbpf/include/bpf/btf.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/uapi/linux/btf.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/src/kernel/bpf/disasm.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/kernel.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/build_bug.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/compiler.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/compiler_types.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/compiler-gcc.h \
+ /home/ubuntu-18/container_escape_detection/bpftool/include/linux/stringify.h \
+ json_writer.h main.h \
+ /home/ubuntu-18/container_escape_detection/src/file/.output/bpftool/bootstrap/libbpf/include/bpf/hashmap.h \
+ xlated_dumper.h
